8ba1009ba6f120f61fe0af1012107d5d02f54897
Steven Ennis <Steven.otieno1@gmail.com>
2018-10-26 01:12:02 +0300

    [Finishes  #161494793] Add user Registration and login

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api
 # Local imports
 from app.api.v1.views.products import Product, Products
 from app.api.v1.views.sales import Sale, Sales
-# from app.api.v1.views.users import Registration, Login, Allusers
+from app.api.v1.views.users import Registration, Login, Allusers
 
 
 version_1 = Blueprint('api', __name__, url_prefix="/api/v1")
@@ -20,7 +20,7 @@ api.add_resource(Sales, '/sales')
 api.add_resource(Sale, '/sales/<int:id>')
 
 # # # Adding a resource to api for Registration and login
-# api.add_resource(Registration, '/registrations')
-# api.add_resource(Login, '/logins')
+api.add_resource(Registration, '/registrations')
+api.add_resource(Login, '/logins')
 
-# api.add_resource(Allusers, '/users')
+api.add_resource(Allusers, '/users')
