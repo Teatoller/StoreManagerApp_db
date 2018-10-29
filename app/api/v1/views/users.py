9eb71dc4372f36b29abd74da1813239b21dc4d91
Steven Ennis <Steven.otieno1@gmail.com>
2018-10-29 06:06:14 +0300

    [Starts #161536330] ft Modify user login and registration Interface responses

--- app/api/v1/views/users.py
+++ app/api/v1/views/users.py
@@ -1,11 +1,11 @@
 from flask_restful import Resource
-from flask import request
+from flask import request, Response
+import json
 from app.api.v1.models.users import UserModel, ListDatabase
 import re
 from werkzeug.security import generate_password_hash, \
      check_password_hash
 
- 
 
 class Registration(Resource):
     def post(self):
@@ -13,22 +13,37 @@ class Registration(Resource):
         valid_username = "".join(data['username'].split())
 
         if len(valid_username) < 6:
-            return {"msg": " username is less than six characters"}, 406
+            invalidUsernameErrorMsg = {
+                "error": "Invalid username passed in request",
+                "helpString": "Username less than six char data to be passed similar to this {'username': 'steven'}"
+                }
+            response = Response(json.dumps(invalidUsernameErrorMsg), status=400, mimetype='application/json')
+            return response
+
         username = data['username']
         
         if not re.match("^[a-zA-Z0-9_]*$", username):
-            return {"msg": " username not valid"}, 406
-    
+            invalidUsernameErrorMsg = {
+                "error": "Invalid username passed in request",
+                "helpString": "Username cannot have special characters e.g # * & data to be passed similar to this {'username': 'steven'}"
+                }
+            response = Response(json.dumps(invalidUsernameErrorMsg), status=400, mimetype='application/json')
+            return response
+
         email = data['email']     
         if not re.match(r"^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$", email):
-            return {"msg":" Email not valid"}, 406
-              
-        user = UserModel(
-            data['username'],
-            data['email'],
-            data['password']
-            )        
-
+            invalidEmailErrorMsg = {
+                "error": "Invalid email passed in request",
+                "helpString": "Email must have @ characters, data to be passed similar to this {'email': 'Jessica@storeapp.co.ke'}"
+                }
+            response = Response(json.dumps(invalidEmailErrorMsg), status=400, mimetype='application/json')
+            return response
+      
+            user = UserModel(
+                data['username'],
+                data['email'],
+                data['password']
+            )
         ListDatabase.USERS.append(user)
         response = user.resultant()
         return {'status': 'Registration Successful', "user": response}, 201
