745f283247e39c61247a1a3463516395aecb510f
Steven Ennis <Steven.otieno1@gmail.com>
2018-10-30 17:11:30 +0300

    [#161569329] Register v2 as version_2 in blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from Instance.config import app_config
-from db import salestable, productstable, userstable
+from db import create_tables
 
 # creating the app
 
@@ -9,11 +9,15 @@ def create_app(config_name='development'):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     app.url_map.strict_slashes = False
-    # salestable()
-    # productstable()
-    # userstable()
+
+    ''' method to create all tables '''
+
+    create_tables()
 
     # Use application to name our blueprint
-    from app.api.v1 import version_1 as v1
-    app.register_blueprint(v1)
+    # from app.api.v1 import version_1 as v1
+    # app.register_blueprint(v1)
+
+    from app.api.v2 import version_2 as v2
+    app.register_blueprint(v2)
     return app
