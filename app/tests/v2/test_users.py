00ee62dd99ac313a37e9eca22c349545f3b6fa83
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-01 13:50:01 +0300

    [#161642802]Add tests for products endpoint

--- app/tests/v2/test_users.py
+++ app/tests/v2/test_users.py
@@ -26,6 +26,61 @@ class TestUsers(unittest.TestCase):
         "password": "test123"        
         }
 
+    invalidsignup_data = {
+        "firstname": "Steven1",
+        "lastname": "Ennis",
+        "username": "Teat#oller",
+        "email": "steven@gmail.co.ke",
+        "password":"test123",
+        "role": "admin"
+        }
+
+    invalidemail_data = {
+        "firstname": "Steven1",
+        "lastname": "Ennis",
+        "username": "Teatoller",
+        "email": "stevengmail.co.ke",
+        "password":"test123",
+        "role": "admin"
+        }
+
+    invalidlenusername_data = {
+        "firstname": "Steven1",
+        "lastname": "Ennis",
+        "username": "Teatr",
+        "email": "stevengmail.co.ke",
+        "password":"test123",
+        "role": "admin"
+        }
+
+    invalidpassword_data = {
+        "firstname": "Steven1",
+        "lastname": "Ennis",
+        "username": "Teatoller",
+        "email": "steven@gmail.co.ke",
+        "password": "test12#3",
+        "role": "admin"
+        }
+
+    invalid_no_username_data = {
+        "firstname": "Steven1",
+        "lastname": "Ennis",
+        "username": " ",
+        "email": "steven@gmail.co.ke",
+        "password": "test12#3",
+        "role": "admin"
+        }
+
+    invalid_no_password_data = {
+        "firstname": "Steven1",
+        "lastname": "Ennis",
+        "username": "Teatoller",
+        "email": "steven@gmail.co.ke",
+        "password": " ",
+        "role": "admin"
+        }
+
+
     def setUp(self):
         """ Method to call up the tests"""
         app.testing = True
@@ -37,6 +92,50 @@ class TestUsers(unittest.TestCase):
                                  data=json.dumps(self.signup_data),
                                  content_type='application/json')
         self.assertEqual(response.status_code, 201)
+
+    def test_invalidsignup_data(self):
+        """ """
+        response = self.app.post(p1_url,
+                                 data=json.dumps(self.invalidsignup_data),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalidemail_data(self):
+        """ """
+        response = self.app.post(p1_url,
+                                 data=json.dumps(self.invalidemail_data),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalidlenusername_data(self):
+        """ """
+        response = self.app.post(p1_url,
+                                 data=json.dumps(self.invalidlenusername_data),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalidpassword_data(self):
+        """ """
+        response = self.app.post(p1_url,
+                                 data=json.dumps(self.invalidpassword_data),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_no_username_data(self):
+        """ """
+        response = self.app.post(p1_url,
+                                 data=json.dumps(self.invalidpassword_data),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_no_password_data(self):
+        """ """
+        response = self.app.post(p1_url,
+                                 data=json.dumps(self.invalid_no_password_data),
+                                 content_type='application/json')
+        self.assertEqual(response.status_code, 400)    
+    
+
 
     def test_Login(self):
         """ """
