ac6419f20fe6746f344b023180b5e836d2ef4ece
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-01 09:33:21 +0300

    [#161639608] Adds single product route

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 # Defines the blueprint application
 
 from app.api.v2.views.users import (Signup, Login)
-from app.api.v2.views.products import (Products)
+from app.api.v2.views.products import (Products, Product)
 
 version_2 = Blueprint('api', __name__, url_prefix="/api/v2")
 api = Api(version_2)
@@ -12,3 +12,4 @@ api = Api(version_2)
 api.add_resource(Signup, '/auth/signup')
 api.add_resource(Login, '/auth/login')
 api.add_resource(Products, '/products')
+api.add_resource(Product, '/products/<int:id>')
