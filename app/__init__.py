cd43cc114e3b1de42349d6e473df1608cd0ea2e3
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-10 20:53:55 +0300

    [#161858120]Add config for blacklist tokens

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from Instance.config import app_config
 from db import create_tables
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (JWTManager, jwt_required, get_raw_jwt)
 
 # creating the app
 
@@ -13,6 +13,9 @@ def create_app(config_name='development'):
     app.config.from_object(app_config[config_name])
     app.url_map.strict_slashes = False
     app.config['JWT_SECRET_KEY'] = "this is my secret"
+    app.config['JWT_BLACKLIST_ENABLED'] = False
+    app.config['JWT_BLACKLIST_TOKENS_CHECKS'] = ['access', 'refresh']
+    jwt = JWTManager(app)
     jwt.init_app(app)
 
     ''' method to create all tables '''
