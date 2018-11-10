173cfec5a184f14911b720801d3d60301a0aa8e6
Steven Ennis <Steven.otieno1@gmail.com>
2018-11-10 20:56:08 +0300

    [#161858120]Add configure logout

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 
 # Defines the blueprint application
 
-from app.api.v2.views.users import (Signup, Login)
+from app.api.v2.views.users import (Signup, Login, Logout)
 from app.api.v2.views.products import Product
 from app.api.v2.views.sales import Sale
 
@@ -12,6 +12,7 @@ api = Api(version_2)
 
 api.add_resource(Signup, '/auth/signup')
 api.add_resource(Login, '/auth/login')
+api.add_resource(Logout, '/auth/logout')
 api.add_resource(Product, '/products', endpoint="get all and post product")
 api.add_resource(Product, '/products/<int:id>', endpoint="specific product")
 api.add_resource(Sale, '/sales', endpoint="get all and post sale")
